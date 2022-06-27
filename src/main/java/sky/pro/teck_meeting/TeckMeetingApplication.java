62e79d8ae2ce6ec4f0b03e4cb54a3738f9e227d8
RickLyudmila <Lrick@yandex.ru>
2022-06-27 16:47:35 +0300

    Initial commit

---
+++ src/main/java/sky/pro/teck_meeting/TeckMeetingApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.teck_meeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeckMeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeckMeetingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/teck_meeting/TeckMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.teck_meeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeckMeetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
